0194e6888745bec21e6570fafe66060a880f5084
Shinichiro Todaka <shinichiro.tdk@gmail.com>
2018-11-29 09:34:43 +0900

    change entrypoint

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,17 @@
-import * as React from "react";
+import * as React from "react"
+import { render } from "react-dom"
+import { Provider } from "react-redux"
+import { ConnectedRouter } from "connected-react-router"
+import { History, createBrowserHistory } from "history"
+
+import { configureStore } from "./store"
+
+const history: History = createBrowserHistory()
+const store = configureStore(history)
+const root = (
+  <Provider store={store}>
+    <ConnectedRouter history={history} />
+  </Provider>
+)
+
+render(root, document.getElementById("root"))
